e78fe4f6103848d3f1fe34b9bc02c8d6e0e7692a
miguelruizc <ruizconde.m@gmail.com>
2024-07-14 11:04:04 +0200

    Add schemas for Items and Categories

---
+++ models/Item.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+	name: { type: String, required: true },
+	description: { type: String, required: true },
+	price: { type: Number, required: true },
+	stock: { type: Number, required: true },
+	category: { type: String, required: true },
+});
+
+ItemSchema.virtual('url').get(function () {
+	return `/items/${this._id}`;
+});
+
+ItemSchema.virtual('formattedPrice').get(function () {
+	return this.price + ' gold';
+});
+
+module.exports = mongoose.model('Item', ItemSchema);
