1acad06d0598b109488f5195ca847fd1178fbbb4
miguelruizc <ruizconde.m@gmail.com>
2024-07-14 13:34:45 +0200

    Add data to /all route

--- routes/all.js
+++ routes/all.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Item = require('../models/Item');
 
-router.get('/', (req, res) => {
-	res.render('all', { title: 'All items' });
+router.get('/', async (req, res) => {
+	const items = await Item.find({});
+
+	if (items) res.render('all', { title: 'All items', items });
+	else console.log('Error, no items');
 });
 
 router.get('/add', (req, res) => {
