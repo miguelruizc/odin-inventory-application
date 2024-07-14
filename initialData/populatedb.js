c699dafe99ec87fe3be0ac11634a48b72a55068b
miguelruizc <ruizconde.m@gmail.com>
2024-07-14 12:20:00 +0200

    Add script to populate initial data in the database

--- initialData/populatedb.js
+++ initialData/populatedb.js
@@ -1,10 +1,53 @@
 const mongoose = require('mongoose');
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const Category = require('../models/Category');
 const Item = require('../models/Item');
 
 mongoose
 	.connect(process.env.MONGODB_URI, {})
 	.then((result) => {
 		console.log('Connected to DB');
+
+		// Read and add categories
+		const categoriesPath = path.join(__dirname, 'categories.json');
+		console.log(categoriesPath);
+		fs.readFile(categoriesPath)
+			.then((data) => {
+				const categories = JSON.parse(data);
+				categories.forEach((category) => {
+					const newCategory = new Category({
+						name: category.name,
+						description: category.description,
+					});
+					newCategory
+						.save()
+						.then((saved) => console.log(`Saved category: ${saved.name}`))
+						.catch((error) => console.log('Error saving category: ' + error));
+				});
+			})
+			.catch((error) => console.log(error));
+
+		// Read and add items
+		const itemsPath = path.join(__dirname, 'items.json');
+		console.log(itemsPath);
+		fs.readFile(itemsPath)
+			.then((data) => {
+				const items = JSON.parse(data);
+				items.forEach((item) => {
+					const newItem = new Item({
+						name: item.name,
+						description: item.description,
+						price: parseInt(item.price),
+						category: item.category,
+						stock: item.stock,
+					});
+					newItem
+						.save()
+						.then((saved) => console.log(`Saved item: ${saved.name}`))
+						.catch((error) => console.log('Error saving item: ' + error));
+				});
+			})
+			.catch((error) => console.log(error));
 	})
 	.catch((error) => console.log(error));
