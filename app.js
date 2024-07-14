79bf96e911ece653141a3b8a40d5dfc6300aec3d
miguelruizc <ruizconde.m@gmail.com>
2024-07-14 10:35:38 +0200

    Add directories and initial mongodb connection

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(3000, () => {
-	console.log('Server running, listening port 3000...');
-});
+mongoose
+	.connect(process.env.MONGODB_URI, {})
+	.then(() => {
+		console.log('Connected to MongoDB');
+		app.listen(3000, () => {
+			console.log('Server running, listening port 3000...');
+		});
+	})
+	.catch((error) => console.error(error));
