0b38e8be53a997ed6c17a20ed073da7f31b9a099
miguelruizc <ruizconde.m@gmail.com>
2024-07-16 11:17:53 +0200

    Add styles

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(hiddenMethodsHandler);
 
+app.use(express.static('public'));
+
 app.use('/categories', categoriesRouter);
 app.use('/equipment', equipmentRouter);
 app.use('/magic-relics', magicRelicsRouter);
